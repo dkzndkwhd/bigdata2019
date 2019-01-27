524826978103042e7cd362d8a68642f15e14ea1a
dkzndkwhd <47074939+dkzndkwhd@users.noreply.github.com>
2019-01-27 17:54:12 +0900

    Add files via upload

--- day01/day01.R
+++ day01/day01.R
@@ -26,9 +26,52 @@ ages[1] <- 20
 # 이름 벡터를 만들고
 # 두번째 사람이 박아무개로 개명
 # 이름의 전체 내용을 출력
-names <- c('john','cindy','walker')
+names <- c('john','cindy','walker','psy')
 names[2] <- '박아무개'
-print(names)
-print(names[2])
+names
 
+# factor 구분 하기
+address <- c("서울","대구","부산","군산","서울","서울","대구")
+address
+str(address)
+# address를 address2 에 factor 로 저장
+address2 <- as.factor(address)
+address2
+address2[1] <- "대구"
+address2
 
+#factor 바꾸기 위해서는 다시 char 로 바꾸어야 한다
+address3 <- as.character(address2)
+address3
+address3[2] <- "광주"
+address3
+
+#입력의 다양한 형태
+x <-  scan() ## 먼저 이거 입력후 콘솔에서 따로 입력.
+x
+
+x2 <- scan()
+x2
+
+x2[2] <- 222
+x2
+str(x2)
+
+# scan 을 문자열로 입력하고 싶다면??
+food <-  scan(what = "") ## 먼저 이거 입력후 콘솔에서 따로 입력.\
+food
+
+str(food)
+class(food)
+typeof(food)
+
+favorite <-  food[1]
+print("내가 제일 좋아하는 음식은" , favorite)
+# 프린트는 하나씩만 인쇄 가능. 파이선처럼 여러개 같이 출력 불가
+
+#여러개 출력시 cat, glue 사용
+cat("내가 제일 좋아하는 음식은" , favorite)
+#glue 는 중간중간에 쑤셔넣을 수 있다. glue는 따로 설치해야 사용가느
+glue("내가 제일 좋아하는 음식은 {favorite}이다" )
+second <-  food[2]
+glue("나의 {second}음식은 2번째로 좋아하는 음식이예요")
