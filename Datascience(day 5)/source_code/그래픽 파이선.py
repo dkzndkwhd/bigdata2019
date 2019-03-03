46f83d7d34d5f093c55f7f0e3038d56b1a9e0048
dkzndkwhd <47074939+dkzndkwhd@users.noreply.github.com>
2019-03-03 14:06:08 +0900

    Add files via upload

---
+++ Datascience(day 5)/source_code/그래픽 파이선.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 파이선 시작 파일
+# 그래픽을 간단하게 파이선으로 구현
+
+
+# In[14]:
+
+
+#1. 필요한 패키지를 설치하기.
+#2. 패키지 사용(그래도 앵간한건 다 설치되어 있음)
+
+from tkinter import * #tkinter 라이브러리에서 모든 기능을 사용하겠다.
+
+
+# In[15]:
+
+
+window = Tk() # 윈도우를 만들어 주는 함수. window는 그저 변수명.  
+
+
+# In[16]:
+
+
+window.title("첫번째 윈도우")# title함수는 윈도우에 제목을 달 수 있는 함수
+
+
+# In[17]:
+
+
+window.geometry("800x600") # 윈도우의 크기를 설정해주는 함수
+
+
+# In[ ]:
+
+
+window.mainloop()
+
+
+# In[ ]:
+
+
+
+
